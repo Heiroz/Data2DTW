653f56a093e671fc2c4dec38f9a498048ff35ee0
hierhier <1849197801@qq.com>
2024-05-06 09:59:11 +0800

    1

--- src/run.py
+++ src/run.py
@@ -4,20 +4,20 @@ import timeseries_raw
 import timeseries_syn
 import dtw
 import show_dtw
-
-raw_file = "../data_in/bilibili_raw.pcap"
-syn_file = "../data_in/bilibili_syn.csv"
-raw_flow_folder = "../data_flow/bilibili_raw"
-syn_flow_folder = "../data_flow/bilibili_syn"
-raw_timeseries_folder = "../data_timeseries/bilibili_raw"
-syn_timeseries_folder = "../data_timeseries/bilibili_syn"
-dtw_folder = "../bilibili_dtw"
-dtw_file = "../bilibili_dtw.csv"
-image_folder = "../bilibili_dtw_image"
+file_name = "live"
+raw_file = "../data_in/{file_name}_raw.pcap"
+syn_file = "../data_in/{file_name}_syn.csv"
+raw_flow_folder = "../data_flow/{file_name}_raw"
+syn_flow_folder = "../data_flow/{file_name}_syn"
+raw_timeseries_folder = "../data_timeseries/{file_name}_raw"
+syn_timeseries_folder = "../data_timeseries/{file_name}_syn"
+dtw_folder = "../{file_name}_dtw"
+dtw_file = "../{file_name}_dtw.csv"
+image_folder = "../{file_name}_dtw_image"
 
 flow_raw.process(raw_file, raw_flow_folder)
-flow_syn.process(syn_file, syn_flow_folder)
-timeseries_raw.process(raw_flow_folder, raw_timeseries_folder, '20ms', 500, 100)
-timeseries_syn.process(syn_flow_folder, syn_timeseries_folder, '20ms', 500, 100)
-dtw.process(syn_timeseries_folder, raw_timeseries_folder, dtw_folder, dtw_file)
-show_dtw.generate(dtw_file, dtw_folder, image_folder, 100)
+# flow_syn.process(syn_file, syn_flow_folder)
+# timeseries_raw.process(raw_flow_folder, raw_timeseries_folder, '20ms', 500, 100)
+# timeseries_syn.process(syn_flow_folder, syn_timeseries_folder, '20ms', 500, 100)
+# dtw.process(syn_timeseries_folder, raw_timeseries_folder, dtw_folder, dtw_file)
+# show_dtw.generate(dtw_file, dtw_folder, image_folder, 100)
