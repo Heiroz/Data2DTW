fb7e4e266adf4ec4737ae38ac93920237b81add0
heiroz <1849197801@qq.com>
2024-05-12 08:11:54 +0000

    11

--- src/run.py
+++ src/run.py
@@ -13,14 +13,14 @@ syn_timeseries_folder = "../data_timeseries/video_syn"
 dtw_folder = "../dtw_file/video_dtw"
 dtw_file = "../dtw/video_dtw.csv"
 image_folder = "../image/video_dtw_image"
-min_packet_len = 100
-min_flow_len = 100
-max_flow_len = 500
-interval = '3000ms'
+min_packet_len = 50
+min_flow_len = 1000
+max_flow_len = 500000
+interval = '100ms'
 
-flow_raw.process(raw_file, raw_flow_folder, min_packet_len)
-flow_syn.process(syn_file, syn_flow_folder, min_packet_len)
+# flow_raw.process(raw_file, raw_flow_folder, min_packet_len)
+# flow_syn.process(syn_file, syn_flow_folder, min_packet_len)
 # timeseries_raw.process(raw_flow_folder, raw_timeseries_folder, interval, max_flow_len, min_flow_len)
 # timeseries_syn.process(syn_flow_folder, syn_timeseries_folder, interval, max_flow_len, min_flow_len)
-# dtw.process(syn_timeseries_folder, raw_timeseries_folder, dtw_folder, dtw_file)
-# show_dtw.generate(dtw_file, dtw_folder, image_folder, 5)
+dtw.process(syn_timeseries_folder, raw_timeseries_folder, dtw_folder, dtw_file)
+show_dtw.generate(dtw_file, dtw_folder, image_folder, 5)
